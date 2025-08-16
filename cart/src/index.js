723d0ff3d83755144cd990e9461c08550e257f8c
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-16 20:05:09 +0700

    cart integration

--- cart/src/index.js
+++ cart/src/index.js
@@ -3,4 +3,5 @@ import faker from "faker";
 const cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
 
 const el = document.getElementById("dev-cart");
+console.log("Trigger");
 el.innerHTML = cartText;
