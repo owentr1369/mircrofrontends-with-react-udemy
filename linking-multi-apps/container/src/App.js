200f9ee20171fe77fe484e7598dec44f9a961408
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-18 17:44:38 +0700

    fixing classname collisions

--- linking-multi-apps/container/src/App.js
+++ linking-multi-apps/container/src/App.js
@@ -2,14 +2,20 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />;
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />;
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
--- linking-multi-apps/marketing/src/App.js
+++ linking-multi-apps/marketing/src/App.js
@@ -4,15 +4,18 @@ import {
   Route,
   BrowserRouter,
 } from "react-router-dom/cjs/react-router-dom.min";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+  });
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing}></Route>
