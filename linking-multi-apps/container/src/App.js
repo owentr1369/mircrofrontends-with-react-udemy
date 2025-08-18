0464b8f34f2c4f1cef32cd3724c08a941142fb47
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-18 17:38:24 +0700

    adding a header

--- linking-multi-apps/container/src/App.js
+++ linking-multi-apps/container/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { mount } from "marketing/MarketingApp";
 import MarketingApp from "./components/MarketingApp";
+import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
-      <h1>Hello there!</h1>
-      <hr />
-      <MarketingApp />;
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />;
+      </div>
+    </BrowserRouter>
   );
 };
