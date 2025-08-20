1c793e91f64f7393d70402102a6d114b28b42800
Tâm Trần Thiện <tamvaa1306@gmail.com>
2025-08-20 14:37:45 +0700

    Finish section 11

--- linking-multi-apps/container/src/App.js
+++ linking-multi-apps/container/src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
-import MarketingApp from "./components/MarketingApp";
-import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Header";
-import { StylesProvider, createGenerateClassName } from "@material-ui/core";
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Progress from './components/Progress';
+import Header from './components/Header';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
-  const generateClassName = createGenerateClassName({
-    productionPrefix: "co",
-  });
   return (
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />;
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
